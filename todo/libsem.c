581fb788b050c5a67a76e98eda4ad001206afcc0
ArmandoVVV <73661094+ArmandoVVV@users.noreply.github.com>
2022-03-26 02:34:10 -0700

    Add files via upload

--- todo/libsem.c
+++ todo/libsem.c
@@ -1,6 +1,9 @@
 #include <pthread_utils.h>
 #include <libsem.h>
 
+int g = 0;
+int g2 = 0;
+
 // Macro que incluye el código de la instrucción máquina xchg
 #define atomic_xchg(A,B) 	__asm__ __volatile__(	\
 							"   lock xchg %1,%0 ;\n"	\
@@ -8,19 +11,51 @@
 							: "m"  (B), "ir" (A)		\
 							);
 
-int g=0;
+
 
 
 void initsem(SEMAPHORE *s,int val)
 {
+	initqueue(&s->queue);
+	s->count = val;
 }
 
 
-void waitsem(SEMAPHORE *s)
-{
+void waitsem(SEMAPHORE *s){
+	int k = 1; 
+
+	while(k != 0){
+		atomic_xchg(k, g);
+	}
+
+	if(s->count > 0){
+		s->count --; 
+	}else{
+		enqueue(&s->queue,pthread_self());
+		s->count--;
+		block_thread();
+	}
+	g = 0;
 }
 
-void signalsem(SEMAPHORE *s)
-{
+void signalsem(SEMAPHORE *s){
+	int k = 1;
+
+	while(k != 0){
+		atomic_xchg(k, g2);
+	}
+
+	if(s->queue.exit == s->queue.entry){
+		s->count++;
+		g2 = 0;
+		return;
+	}
+
+	if(s->count <= 0)
+	{
+		s->count++;
+		unblock_thread(dequeue(&s->queue));
+	}
+	g2 = 0;
 }
 
